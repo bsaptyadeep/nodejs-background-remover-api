21ff6f0d7fe805d9ea89b531eb5f098fa5709b5f
Saptyadeep <bsaptyadeep@gmail.com>
2022-03-26 21:25:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const {removeBackgroundFromImageUrl,removeBackgroundFromImageFile} = require("remove.bg")
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const removeRoute = require("./routes/remove")
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+
+const localFile = "./local/file/name.jpg";
+const outputFile = `${__dirname}/out/img-removed-from-file.png`;
+
+app.use("/remove-back", removeRoute);
+
+const port = process.env.PORT || 8080;
+app.listen(port, console.log(`Listening on port ${port}...`));
---
+++ routes/remove.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+// var atob = require('atob');
+// const Blob = require('node-blob');
+const { removeBackgroundFromImageUrl, removeBackgroundFromImageFile, RemoveBgResult } = require("remove.bg")
+
+router.post("/", async (req, res) => {
+    console.log(req.body)
+    const url = `${req.body.url}`;
+    const outputFile = `${__dirname}/out/output1.png`;
+    console.log(url);
+    removeBackgroundFromImageUrl({
+        url,
+        apiKey: process.env.KEY,
+        size: "regular",
+        type: "person",
+        bg_color: req.body.backgroundColor,
+        bg_image_url: req.body.backgroundUrl,
+        outputFile
+    }).then((result) => {
+        console.log(`File saved to ${outputFile}`);
+        const base64img = result.base64img;
+        return res.status(200).send({ output: base64img })
+    }).catch((er) =>
+    {
+        console.log(er)
+        return res.status(400).send({ error: er })
+    })
+
+})
+
+module.exports = router;
